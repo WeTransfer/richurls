63a3995479405a22ccd1160ab321fa9a869221bc
grdw <gerard@wetransfer.com>
2020-01-08 14:36:58 +0100

    Add a generich rich urls spec

---
+++ spec/lib/rich_urls_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe RichUrls do
+  it 'not a url' do
+    expect { RichUrls.enrich('not-a-url') }
+      .to raise_error(RichUrls::MalformedURLError)
+  end
+
+  it 'enriches a url' do
+    enriched = RichUrls.enrich('https://wetransfer.com')
+
+    expect(enriched).to have_key('title')
+    expect(enriched).to have_key('description')
+    expect(enriched).to have_key('embed')
+    expect(enriched).to have_key('image')
+  end
+end
