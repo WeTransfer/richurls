881700e791bd59daac807c98d0d3de57fc1c6af3
grdw <gerard@wetransfer.com>
2020-01-09 10:11:17 +0100

    Simplify embed parsers

--- lib/parsers/embed_parser.rb
+++ lib/parsers/embed_parser.rb
@@ -1,16 +1,38 @@
 module Parsers
-  require_relative 'embed_parser/base'
-  require_relative 'embed_parser/youtube'
-  require_relative 'embed_parser/paste'
+  IFRAMES = {
+    youtube: '<iframe width="560" height="315" src="%s" frameborder="0" '\
+             'allow="accelerometer; autoplay; encrypted-media; '\
+             'gyroscope; picture-in-picture" allowfullscreen>'\
+             '</iframe>',
+    paste: '<iframe src="%s" width="480" height="480" scrolling="no" '\
+           'frameborder="0" allowfullscreen></iframe>'
+  }.freeze
+
+  EMBED_CONFIG = {
+    /http(s)?\:\/\/(www\.)?youtube\.com\// => {
+      template: :youtube,
+      url_sub: [/watch\?v=/, 'embed/']
+    },
+    /http(s)?\:\/\/youtu\.be\// => {
+      template: :youtube,
+      url_sub: [/youtu\.be\/(.+)/, 'www.youtube.com/embed/\1']
+    },
+    /http(s)?\:\/\/(www\.)?pasteapp\.com\// => {
+      template: :paste,
+      url_sub: [/\?view=/, '/embed?view=']
+    }
+  }.freeze
 
   class EmbedParser
     def self.call(_, url)
-      case url
-      when Youtube::URL
-        Youtube.generate_embed(url)
-      when Paste::URL
-        Paste.generate_embed(url)
+      result = nil
+      EMBED_CONFIG.each do |url_regex, template|
+        next unless url =~ url_regex
+
+        result = IFRAMES[template[:template]] % url.sub(*template[:url_sub])
+        break
       end
+      result
     end
   end
 end
--- spec/lib/embed_spec.rb
+++ spec/lib/embed_spec.rb
@@ -1,25 +1,14 @@
 require 'spec_helper'
 
-RSpec.describe RichUrls::BodyDecorator do
-  describe 'embed decorator' do
-    describe 'youtube' do
-      let(:file) { File.read('./spec/fixtures/youtube_video.html') }
+RSpec.describe Parsers::EmbedParser do
+  let(:result) { Parsers::EmbedParser.call(nil, url) }
 
-      let(:result) {
-        decorator = RichUrls::BodyDecorator.new(
-          'https://www.youtube.com/watch?v=ONYyFiKjJ20',
-          file
-        )
-
-        result = decorator.decorate
-      }
-
-      it 'provider display matches' do
-        expect(result['provider_display']).to eq('https://www.youtube.com')
-      end
+  describe 'embed parser' do
+    describe 'youtube full link' do
+      let(:url) { 'https://www.youtube.com/watch?v=ONYyFiKjJ20' }
 
       it 'fetches the embed code from a page like Youtube' do
-        expect(result['embed']).to eq(
+        expect(result).to eq(
           '<iframe width="560" height="315" '\
           'src="https://www.youtube.com/embed/ONYyFiKjJ20" frameborder="0" '\
           'allow="accelerometer; autoplay; encrypted-media; gyroscope; '\
@@ -28,21 +17,30 @@ RSpec.describe RichUrls::BodyDecorator do
       end
     end
 
-    it 'fetches the embed code from a page like Paste' do
-      file = File.read('./spec/fixtures/paste_slideshow.html')
-      decorator = RichUrls::BodyDecorator.new(
-        'https://pasteapp.com/p/jbYfTeB8726?view=xhund8bMW4W',
-        file
-      )
-      result = decorator.decorate
+    describe 'youtube short link' do
+      let(:url) { 'https://youtu.be/4gLrlolyDag' }
 
-      expect(result['embed']).to eq(
-        '<iframe '\
-        'src="https://pasteapp.com/p/jbYfTeB8726/embed?view=xhund8bMW4W" '\
-        'width="480" height="480" scrolling="no" frameborder="0" '\
-        'allowfullscreen></iframe>'
-      )
+      it 'fetches the embed code from a page like Youtube' do
+        expect(result).to eq(
+          '<iframe width="560" height="315" '\
+          'src="https://www.youtube.com/embed/4gLrlolyDag" frameborder="0" '\
+          'allow="accelerometer; autoplay; encrypted-media; gyroscope; '\
+          'picture-in-picture" allowfullscreen></iframe>'
+        )
+      end
+    end
+
+    context 'paste' do
+      let(:url) { 'https://pasteapp.com/p/jbYfTeB8726?view=xhund8bMW4W' }
+
+      it 'fetches the embed code from a page like Paste' do
+        expect(result).to eq(
+          '<iframe '\
+          'src="https://pasteapp.com/p/jbYfTeB8726/embed?view=xhund8bMW4W" '\
+          'width="480" height="480" scrolling="no" frameborder="0" '\
+          'allowfullscreen></iframe>'
+        )
+      end
     end
   end
-
 end
