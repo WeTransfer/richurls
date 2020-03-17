87321d2de9f054d80b17b411b5aa750b593583b1
grdw <gerard@wetransfer.com>
2020-03-17 14:05:31 +0100

    Implement QueryInterface

--- lib/xml_handler.rb
+++ lib/xml_handler.rb
@@ -1,64 +1,32 @@
+require_relative 'query'
+require_relative 'query_interface'
+
 module RichUrls
   class XMLHandler < ::Ox::Sax
-    WHITELISTED_EL_NAMES = %i[
-      title
-      meta
-      script
-      link
-      img
-      p
-    ].freeze
-
-    WHITELISTED_ATTRS = %i[
-      property
-      content
-      rel
-      href
-      src
-    ].freeze
-
-    StopParsingError = Class.new(StandardError)
-    El = Struct.new(:name, :attributes)
-
-    attr_accessor :elements
-
     def initialize
-      @elements = []
+      @query_interface = QueryInterface.new
     end
 
-    def find(name, attrs = {})
-      @elements.detect do |el|
-        matching_attributes = attrs.all? { |k, v| el.attributes[k] == v }
-
-        el.name == name && matching_attributes
-      end
-    end
-
-    def start_element(element_name)
-      return unless WHITELISTED_EL_NAMES.include?(element_name)
-
-      @elements << El.new(element_name, {})
-    end
-
-    def attr(name, str)
-      el = @elements.last
-
-      return unless el && WHITELISTED_ATTRS.include?(name)
-
-      el.attributes[name] = str
-
-      raise StopParsingError if stop?(name, el)
-    end
-
-    def text(str)
-      el = @elements.last
-      el && el.attributes[:text].nil? && el.attributes[:text] = str
+    def elements
+      @query_interface.elements
     end
 
     private
 
-    def stop?(name, elem)
-      name == WHITELISTED_ATTRS.last && elem.name == :img
+    def start_element(element_name)
+      @query_interface.start(element_name)
+    end
+
+    def end_element(element_name)
+      @query_interface.end(element_name)
+    end
+
+    def attr(name, str)
+      @query_interface.attr(name, str)
+    end
+
+    def text(str)
+      @query_interface.text(str)
     end
   end
 end
